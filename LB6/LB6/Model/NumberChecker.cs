5cf2c907881c081e72a301bf0f43416e915670a1
Din345RTETYETYU <168690827+Din345RTETYETYU@users.noreply.github.com>
2024-05-02 10:05:18 +1000

    Add files via upload

---
+++ LB6/LB6/Model/NumberChecker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB6.Model
+{
+    public class NumberChecker
+    {
+        public bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
---
+++ LB6/LB6/ViewModels/MyViewModel.cs
@@ -0,0 +1,67 @@
+﻿using LB6;
+using System.ComponentModel;
+using System.Windows.Input;
+using LB6.Model; 
+
+namespace LB6.ViewModels
+{
+    public class MyViewModel : INotifyPropertyChanged
+    {
+        private int inputNumber;
+        private string resultMessage;
+        private NumberChecker numberChecker = new NumberChecker(); 
+
+        public int InputNumber
+        {
+            get { return inputNumber; }
+            set
+            {
+                if (inputNumber != value)
+                {
+                    inputNumber = value;
+                    RaisePropertyChanged("InputNumber");
+                }
+            }
+        }
+
+        public string ResultMessage
+        {
+            get { return resultMessage; }
+            set
+            {
+                if (resultMessage != value)
+                {
+                    resultMessage = value;
+                    RaisePropertyChanged("ResultMessage");
+                }
+            }
+        }
+
+        public ICommand CheckEvenCommand { get; }
+
+        public MyViewModel()
+        {
+            CheckEvenCommand = new RelayCommand(ExecuteCheckEven);
+        }
+
+        private void ExecuteCheckEven(object parameter)
+        {
+            if (InputNumber < 0 || InputNumber > 100)
+            {
+                ResultMessage = "Введите число от 0 до 100.";
+            }
+            else
+            {
+                bool isEven = numberChecker.IsEven(InputNumber);
+                ResultMessage = isEven ? "Число четное." : "Число нечетное.";
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
